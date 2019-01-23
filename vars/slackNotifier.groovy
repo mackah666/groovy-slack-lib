cfaa1a6a68792f448418ab93876256d3b2eae943
mackah666 <mackah666@yahoo.co.uk>
2019-01-23 13:29:45 +0000

    Added build url

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
+def call(String buildResult, String changeLog) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at ${env.BUILD_URL} was successful"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at Url: ${env.BUILD_URL} was successful ChangeLog: ${changeLog}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at ${env.BUILD_URL} was failed"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at Url: ${env.BUILD_URL} was failed"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at ${env.BUILD_URL} was unstable"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at Url: ${env.BUILD_URL} was unstable"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at ${env.BUILD_URL} its result was unclear"	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at Url: ${env.BUILD_URL} its result was unclear"	
   }
 }
