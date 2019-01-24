cde2448bd816f136eb325ed7f83d8141fdcc5995
michael ackah <mackah666@yahoo.co.uk>
2019-01-24 10:30:22 +0000

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -3,13 +3,7 @@
 def call(String buildResult, String changeLog) {
   
   changeLog =  { URLDecoder.decode(changeLog) }
-  changeLog = changeLog.replace("\n", "\\n")
-  changeLog = changeLog.replace("\t", "\\t")
-  changeLog = changeLog.replace("\"", '\\"')
-  changeLog = changeLog.replace("&", "&amp;")
-  changeLog = changeLog.replace("<", "&lt;")
-  changeLog = changeLog.replace(">", "&gt;")
-
+  
   if ( buildResult == "SUCCESS" ) {
     slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} at Url: ${env.BUILD_URL} was successful ChangeLog: $changeLog"
   }
